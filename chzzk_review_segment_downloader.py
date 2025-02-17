f943fa52fd1fa58981b27efdb3f7d55248cb88c5
replica <60418809+junobonnie@users.noreply.github.com>
2025-02-17 12:17:13 +0900

    Add files via upload

---
+++ chzzk_review_segment_downloader.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 21:31:20 2025
+
+@author: replica
+"""
+
+# @title 필요 라이브러리 불러오기
+import re
+import json
+import requests
+import xml.etree.ElementTree as ET
+import time
+import subprocess
+import os
+import sys
+
+def get_ffmpeg_path():
+    if getattr(sys, 'frozen', False):  # 실행 파일로 빌드된 경우
+        return os.path.join(sys._MEIPASS, "ffmpeg_bin", "ffmpeg.exe")
+    return "ffmpeg"  # 개발 중에는 시스템 FFmpeg 사용
+
+class ChzzkStreamExtractor:
+    VOD_URL = "https://apis.naver.com/neonplayer/vodplay/v2/playback/{video_id}?key={in_key}"
+    VOD_INFO = "https://api.chzzk.naver.com/service/v2/videos/{video_no}"
+    
+    def __init__(self, message = print, progress_update = (lambda x: None)):
+        self.message = message
+        self.progress_update = progress_update
+        self.NID_AUT = ""
+        self.NID_SES = ""
+        self.download_path = "./"
+
+    def extract_streams(self, link, start_time, end_time, start_time_str, end_time_str, download_path):
+        if not download_path == "":
+            self.download_path = download_path
+        match = re.match(r'https?://chzzk\.naver\.com/(?:video/(?P<video_no>\d+)|live/(?P<channel_id>[^/?]+))$', link)
+        if not match:
+            self.message("Invalid link\n")
+            return
+
+        video_no = match.group("video_no")
+        return self._get_vod_streams(video_no, start_time, end_time, start_time_str, end_time_str)
+
+    def download_video(self, video_url, output_path, start_time, end_time):
+        total_duration = end_time - start_time
+        if total_duration <= 0:
+            self.message("잘못된 구간입니다. (end_time이 start_time보다 커야 함)")
+            return
+    
+        command = [
+            get_ffmpeg_path(), "-y", "-ss", str(start_time), "-i", video_url,
+            "-to", str(end_time - start_time), "-c", "copy", output_path,
+            "-progress", "pipe:2", "-nostats"
+        ]
+    
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+            text=True,  # Python 3.7 이상에서는 universal_newlines=True 대신 사용
+            bufsize=1,  # 줄 단위로 즉시 출력
+            encoding="utf-8",
+            creationflags=0x08000000
+        )
+    
+        while True:
+            if process.poll() is not None:
+                break
+    
+            line = process.stderr.readline()
+            if not line:
+                time.sleep(0.1)
+                continue
+    
+            decoded_line = line.strip()
+    
+            if 'out_time_ms=' in decoded_line:
+                match = re.search(r'out_time_ms=(\d+)', decoded_line)
+                if match:
+                    current_time = int(match.group(1)) / 1_000_000
+                    if current_time > total_duration:
+                        current_time = total_duration
+                    progress = (current_time / total_duration) * 100
+                    self.progress_update(progress)
+                    self.message(f"\rDownloading... {progress:.2f}% ")
+                    # sys.stdout.flush()  # 추가
+    
+        process.wait()
+        self.message("\nDownload completed!\n")
+
+
+    def _print_dash_manifest(self, video_url):
+        try:
+            response = requests.get(video_url, headers={"Accept": "application/dash+xml"})
+            response.raise_for_status()
+            root = ET.fromstring(response.text)
+            ns = {"mpd": "urn:mpeg:dash:schema:mpd:2011", "nvod": "urn:naver:vod:2020"}
+            base_url_element = root.find(".//mpd:BaseURL", namespaces=ns)
+            if base_url_element is not None:
+                return base_url_element.text
+            else:
+                self.message("BaseURL not found in DASH manifest\n")
+        except requests.RequestException as e:
+            self.message("Failed to fetch DASH manifest:" + str(e) + "\n")
+        except ET.ParseError as e:
+            self.message("Failed to parse DASH manifest XML:" + str(e) + "\n")
+
+    def _get_vod_streams(self, video_no, start_time, end_time, start_time_str, end_time_str):
+        api_url = self.VOD_INFO.format(video_no=video_no)
+        UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+        try:
+            if self.NID_AUT == ""  or self.NID_SES =="":
+                response = requests.get(api_url, headers={"User-Agent": UserAgent})
+            else:
+                response = requests.get(api_url, cookies={"NID_AUT":self.NID_AUT, "NID_SES":self.NID_SES}, headers={"User-Agent": UserAgent})
+            response.raise_for_status()
+        except requests.RequestException as e:
+            self.message("Failed to fetch video information:" + str(e) + "\n")
+            return
+
+        if response.status_code == 404:
+            self.message("Video not found\n")
+            return
+
+        try:
+            content = response.json().get('content', {})
+            video_id = content.get('videoId')
+            in_key = content.get('inKey')
+            if video_id is None or in_key is None:
+                self.message("This is a need to login video." + "\n")
+                cookies = self._load_cookies_from_file("cookies.json")
+                if cookies is not None:
+                    response = requests.get(api_url, cookies=cookies, headers={"User-Agent": UserAgent})
+                    response.raise_for_status()
+                    content = response.json().get('content', {})
+                    video_id = content.get('videoId')
+                    in_key = content.get('inKey')
+
+            video_url = self.VOD_URL.format(video_id=video_id, in_key=in_key)
+            author = content.get('channel', {}).get('channelName')
+            category = content.get('videoCategory')
+            title = content.get('videoTitle')
+            self.message(f"Author: {author}, Title: {title}, Category: {category}")
+            base_url = self._print_dash_manifest(video_url)
+            if base_url:
+                title = self.clean_filename(title)
+                output_path = f"{self.download_path}/{title}_{start_time_str.replace(':', '-')}_{end_time_str.replace(':', '-')}.mp4"
+                self.download_video(base_url, output_path, start_time, end_time)
+                # self.move_to_drive(output_path)
+        except json.JSONDecodeError as e:
+            self.message("Failed to decode JSON response:" + str(e))
+
+    def _load_cookies_from_file(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                cookies = json.load(file)
+            return cookies
+        except FileNotFoundError:
+            self.message(f"Cookie file not found: {file_path}" + "\n")
+            return None
+        except json.JSONDecodeError:
+            self.message(f"Error decoding JSON from file: {file_path}" + "\n")
+            return None
+
+    @staticmethod
+    def clean_filename(filename):
+        cleaned_filename = re.sub(r'[♥♡ღ⭐㉦✧》《♠♦❤️♣✿ꈍᴗ\/@!~*\[\]\#\$\%\^\&\(\)\-\_\=\+\<\>\?\;\:\'\"]', '', filename)
+        return cleaned_filename
+
+    # def move_to_drive(self, file_path):
+    #     drive_path = './'
+    #     if not os.path.exists(drive_path):
+    #         os.makedirs(drive_path)
+    #     file_name = os.path.basename(file_path)
+    #     drive_file_path = os.path.join(drive_path, file_name)
+    #     shutil.move(file_path, drive_file_path)
+    #     self.message(f"File moved to: {drive_file_path}")
+
+def convert_time_to_seconds(time_str, message = print):
+    try:
+        h, m, s = map(int, time_str.split(':'))
+        return h * 3600 + m * 60 + s
+    except ValueError:
+        message("Invalid time format. Use hh:mm:ss")
+        return None
+
+# link_input = widgets.Text(
+#     value='',
+#     placeholder='Enter the link',
+#     description='Link:',
+#     disabled=False
+# )
+
+# start_time_input = widgets.Text(
+#     value='00:00:00',
+#     placeholder='Start time in hh:mm:ss',
+#     description='Start Time:',
+#     disabled=False
+# )
+
+# end_time_input = widgets.Text(
+#     value='00:01:00',
+#     placeholder='End time in hh:mm:ss',
+#     description='End Time:',
+#     disabled=False
+# )
+
+# download_button = widgets.Button(
+#     description='Download',
+#     disabled=False,
+#     button_style='',
+#     tooltip='Click to download video',
+#     icon='download'
+# )
+
+# display(link_input, start_time_input, end_time_input, download_button)
+
+# def on_download_button_clicked(b):
+#     link = link_input.value
+#     start_time_str = start_time_input.value
+#     end_time_str = end_time_input.value
+#     start_time = convert_time_to_seconds(start_time_str)
+#     end_time = convert_time_to_seconds(end_time_str)
+#     if link and start_time is not None and end_time is not None:
+#         ChzzkStreamExtractor.extract_streams(link, start_time, end_time, start_time_str, end_time_str)
+#     else:
+#         print("Please provide valid inputs")
+
+# download_button.on_click(on_download_button_clicked)
+
+if __name__ == "__main__":
+    CSE = ChzzkStreamExtractor()
+    CSE.NID_AUT, CSE.NID_SES = "", ""
+    link = "https://chzzk.naver.com/video/5845060"
+    start_time_str = "01:00:05"
+    end_time_str = "01:00:55"
+    start_time = convert_time_to_seconds(start_time_str)
+    end_time = convert_time_to_seconds(end_time_str)
+    if link and start_time is not None and end_time is not None:
+        CSE.extract_streams(link, start_time, end_time, start_time_str, end_time_str, "")
+    else:
+        print("Please provide valid inputs")
+    
---
+++ gui.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 00:24:47 2025
+
+@author: replica
+"""
+import chzzk_review_segment_downloader as crsd
+import customtkinter as ctk
+from tkinter import filedialog
+import os
+import threading
+import webbrowser
+
+def on_button_click():
+    def task():
+        button.configure(state="disabled")
+        url = entry.get()
+        CSE.NID_AUT, CSE.NID_SES = get_NID()
+        
+        download_path = path_entry.get()
+        
+        start_time_str = start_entry.get()
+        end_time_str = end_entry.get()
+        start_time = crsd.convert_time_to_seconds(start_time_str, add_log)
+        end_time = crsd.convert_time_to_seconds(end_time_str, add_log)
+        if url and start_time is not None and end_time is not None:
+            CSE.extract_streams(url, start_time, end_time, start_time_str, end_time_str, download_path)
+        else:
+            add_log("Please provide valid inputs")
+        button.configure(state="normal")
+
+    thread = threading.Thread(target=task)
+    thread.start()
+    
+def save_download_path(path: str):
+    with open("config", "w") as f:
+        f.write(path)
+
+def load_download_path():
+    with open("config", "r") as f:
+        path = f.read()
+        if not path == "":
+            path_entry.insert(0, path)
+            
+def select_download_path():
+    init_path = path_entry.get()
+    if init_path == "":
+        init_path = os.getcwd()
+        
+    path = filedialog.askdirectory(title = "다운로드 폴더 선택창", initialdir = init_path)
+    if path:
+        path_entry.delete(0, "end")
+        path_entry.insert(0, path)
+        save_download_path(path)
+
+def open_download_path():
+    path = path_entry.get()
+    if path == "":
+        path = os.getcwd()
+    
+    if os.path.isdir(path):
+        os.startfile(path)
+
+def open_github():
+    github_url = "https://github.com/junobonnie/chzzk_review_segment_downloader"  # 이동할 GitHub 페이지 URL
+    webbrowser.open(github_url)  # 기본 브라우저로 URL 열기
+
+def get_NID():
+    with open("NID.txt", "r") as f:
+        NID = f.read().split(" ")
+    if not len(NID) == 2:
+        NID = ["", ""]
+    return NID
+
+def get_popup_pos(popup_width, popup_height):
+    root_x = root.winfo_x()  # 메인 창의 X 좌표
+    root_y = root.winfo_y()  # 메인 창의 Y 좌표
+    root_width = root.winfo_width()  # 메인 창의 너비
+    root_height = root.winfo_height()  # 메인 창의 높이
+
+    # 팝업 창 위치 계산 (메인 창 중심)
+    popup_x = root_x + (root_width // 2) - (popup_width // 2)
+    popup_y = root_y + (root_height // 2) - (popup_height // 2)
+    return popup_x, popup_y
+
+# 팝업창 생성 함수
+def open_popup():
+    # 팝업 창 생성
+    popup_width, popup_height = 250, 150
+    popup_x, popup_y = get_popup_pos(popup_width, popup_height)
+    
+    popup = ctk.CTkToplevel(root)
+    popup.geometry(f"{popup_width}x{popup_height}+{popup_x}+{popup_y}")
+        
+    popup.title("NID 수정")
+    popup.grab_set()  # 모달 창으로 설정 (메인 창 비활성화)
+
+    NID_AUT, NID_SES = get_NID()
+
+    # 라벨과 입력창을 같은 행에 배치
+    input_area = ctk.CTkFrame(popup, width=250, corner_radius=0)
+    input_area.pack()
+    
+    label_prompt1 = ctk.CTkLabel(input_area, text="NID_AUT")
+    label_prompt1.grid(row=0, column=0, padx=10, pady=10, sticky="w")  # 좌측 정렬
+    
+    entry1 = ctk.CTkEntry(input_area, width=150, placeholder_text=NID_AUT)
+    entry1.grid(row=0, column=1, padx=10, pady=10)
+    
+    label_prompt2 = ctk.CTkLabel(input_area, text="NID_SES")
+    label_prompt2.grid(row=1, column=0, padx=10, pady=10, sticky="w")  # 좌측 정렬
+    
+    entry2 = ctk.CTkEntry(input_area, width=150, placeholder_text=NID_SES)
+    entry2.grid(row=1, column=1, padx=10, pady=10)
+    
+    def save_NID():
+        NID_AUT = entry1.get()  # 입력 필드의 텍스트 가져오기
+        NID_SES = entry2.get()
+        
+        if not (NID_AUT == "" and NID_SES == ""):
+            with open("NID.txt", "w") as f:
+                f.write(NID_AUT+" "+NID_SES)
+                
+            popup_width, popup_height = 100, 50
+            popup_x, popup_y = get_popup_pos(popup_width, popup_height)
+            
+            new_popup = ctk.CTkToplevel(root)
+            new_popup.geometry(f"{popup_width}x{popup_height}+{popup_x}+{popup_y}")
+            new_popup.title("")
+            new_popup.grab_set()
+            label = ctk.CTkLabel(new_popup, text="저장완료")
+            label.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
+        
+    button0 = ctk.CTkButton(popup, text="저장하기", command=save_NID)
+    button0.pack(padx=10, pady=10)
+    
+# 새로운 텍스트 추가 함수
+def add_log(text):
+    scrollable_log.configure(state="normal")  # 편집 가능하게 설정
+    scrollable_log.insert("end", "\n\n%s"%(text))  # 새로운 텍스트 추가
+    scrollable_log.configure(state="disabled")  # 다시 편집 불가능하게 설정
+    scrollable_log.yview_moveto(1.0)  # 스크롤을 가장 아래로 이동
+
+def progress_update(value):
+    progress_bar.set(value/100)
+
+CSE = crsd.ChzzkStreamExtractor(add_log, progress_update)
+
+# UI 초기화
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+
+root = ctk.CTk()
+root.title("치지직 다시보기 구간 다운로더")
+root.geometry("500x200")
+root.iconbitmap("icon.ico")
+
+# 좌측 메뉴바 생성
+menu_bar = ctk.CTkFrame(root, width=100, corner_radius=0)
+menu_bar.pack(side="left", fill="y")
+
+info_button = ctk.CTkButton(menu_bar, text="프로그램 정보", width=100, command=open_github)
+info_button.pack(pady=10, padx=10, fill="x")
+
+NID_button = ctk.CTkButton(menu_bar, text="NID 수정", width=100, command=open_popup)
+NID_button.pack(pady=10, padx=10, fill="x")
+
+# 스크롤 가능한 로그
+scrollable_log = ctk.CTkTextbox(menu_bar, width=100, wrap="word")
+scrollable_log.insert("1.0", "Hello!")
+scrollable_log.configure(state="disabled")  # 편집 불가능하게 설정
+scrollable_log.pack(pady=10)
+
+# URL 입력 필드
+entry = ctk.CTkEntry(root, placeholder_text="다시보기 방송주소를 입력하세요")
+entry.pack(pady=10, padx=20, fill='x')
+
+# 시간 입력 필드
+time_frame = ctk.CTkFrame(root)
+time_frame.pack(pady=10, padx=20, fill='x')
+
+start_entry = ctk.CTkEntry(time_frame, placeholder_text="00:00:00", width=50)
+start_entry.pack(side="left", fill='x', expand=True, padx=5)
+
+# 라벨 생성
+label = ctk.CTkLabel(time_frame, text="~")
+label.pack(side="left", padx=5)
+
+end_entry = ctk.CTkEntry(time_frame, placeholder_text="00:00:00", width=50)
+end_entry.pack(side="right", fill='x', expand=True, padx=5)
+
+# 다운로드 경로 입력 필드 및 버튼
+path_frame = ctk.CTkFrame(root)
+path_frame.pack(pady=10, padx=20, fill='x')
+
+path_entry = ctk.CTkEntry(path_frame, placeholder_text="다운로드 경로를 선택하세요", width=200)
+path_entry.pack(side="left", fill='x', expand=True, padx=5)
+
+load_download_path()
+
+path_button = ctk.CTkButton(path_frame, text="찾기", command=select_download_path, width=20)
+path_button.pack(side="left", padx=5)
+
+open_button = ctk.CTkButton(path_frame, text="열기", command=open_download_path, width=20)
+open_button.pack(side="right", padx=5)
+
+# 다운로드 버튼
+download_frame = ctk.CTkFrame(root)
+download_frame.pack(pady=10, padx=20, fill='x')
+
+# 진행 상태 바
+progress_bar = ctk.CTkProgressBar(download_frame)
+progress_bar.pack(side="left", fill='x', expand=True, padx=5)
+progress_bar.set(0)
+
+
+# 확인 버튼 및 진행 상태 바 배치
+button = ctk.CTkButton(download_frame, text="다운로드", command=on_button_click, width=90)
+button.pack(side="right", padx=5)
+
+# 실행
+root.mainloop()
